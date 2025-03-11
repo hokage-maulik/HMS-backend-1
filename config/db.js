76e4f7b6f7ee46baf84537ca15d3eb83a2a2bb33
hokage-maulik <maulikvaghela899@gmail.com>
2025-03-11 15:30:27 +0530

    okok

--- config/db.js
+++ config/db.js
@@ -1,9 +1,26 @@
-const mongoose=require("mongoose")
+// const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/Project1")
+// mongoose.connect("mongodb://localhost:27017/Project1")
 
-const db=mongoose.connection
+// const db=mongoose.connection
 
-db.on("connected",()=>{
-    console.log("connected to database")
-})
+// db.on("connected",()=>{
+//     console.log("connected to database")
+// })
+
+
+require('dotenv').config(); // Load environment variables
+
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/Project1"; // Fallback to localhost for local development
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Error connecting to MongoDB", err));
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("connected to database");
+});
